c9ed9d6501c93ea66e39b0d5385eb30acf561e7b
Wesley Chioh <wes.chioh@outlook.com>
2020-04-15 18:02:23 +0800

    PCA and cosine similarity of KJV done

--- EDa.R
+++ EDa.R
@@ -10,6 +10,20 @@ gospels <- all_bibles %>%
 gospels <- gospels %>%
   mutate(Book = str_split(Verse, " ")[[1]][1])
 
+# just the KJV
+all_bibles <- all_bibles %>%
+  mutate(Verse = str_replace_all(Verse, c("1 " = "First", "2 " = "Second", "3 " = "Third")))
+books <- sapply(1:dim(all_bibles)[1], function(x){
+  book = str_split(all_bibles[x,1], " ")[[1]][1]
+  book
+})
+all_bibles$Book <- books
+
+# collapsing into each book
+kjv_collapsed <- all_bibles %>%
+  select(Book, `King James Bible`) %>%
+  group_by(Book) %>%
+  summarise(Verse = paste(`King James Bible`, collapse = " "))
 
 # selecting the relevant versions
 kjv_gospels <- gospels %>%
